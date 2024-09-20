05c53f265ae416a7cf20c882071b7be2b6074456
ANUSHREE SRIVASTAVA <133258383+Anushree2702@users.noreply.github.com>
2024-09-20 15:04:48 +0530

    Add files via upload

---
+++ Array/Trapping_rain_water.java
@@ -0,0 +1,33 @@
+package Array;
+
+public class Trapping_rain_water {
+    public static void main(String[] args) {
+        int[] height={0,1,0,2,1,0,1,3,2,1,2,1};
+        System.out.println(rain(height));
+        
+    }
+    static int rain(int[] height)
+    {
+        int n=height.length;
+        int[] left=new int[n];
+        int[] right=new int[n];
+        left[0]=height[0];
+        for(int i=1;i<n;i++)
+        {
+            left[i]=Math.max(left[i-1],height[i]);
+
+        }
+        right[n-1]=height[n-1];
+        for(int i=n-2;i>=0;i--)
+        {
+            right[i]=Math.max(right[i+1],height[i]);
+        }
+        int ans=0;
+        for(int i=0;i<n;i++)
+        {
+            ans=ans+Math.min(left[i],right[i])-height[i];
+        }
+        return ans;
+    }
+    
+}
