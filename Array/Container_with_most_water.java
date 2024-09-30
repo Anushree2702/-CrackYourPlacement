8847838f55dcafa29cab728ca29dd2e8e0ab05e6
ANUSHREE SRIVASTAVA <133258383+Anushree2702@users.noreply.github.com>
2024-09-30 22:56:09 +0530

    Update Container_with_most_water.java

--- Array/Container_with_most_water.java
+++ Array/Container_with_most_water.java
@@ -1,10 +1,13 @@
 package Array;
 
 public class Container_with_most_water {
+    
     public static void main(String[] args) {
         int[] height={1,8,6,2,5,4,8,3,7};
         System.out.println(maxArea(height));
     }
+    
+    
     static int maxArea(int[] height) {
         int start=0;
         int end=height.length-1;
